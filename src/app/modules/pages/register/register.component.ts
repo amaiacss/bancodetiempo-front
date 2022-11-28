df5dddc929dbe08eea673dc794ce42462cabb82f
nereVega <nvega@birt.eus>
2022-11-28 19:36:05 +0100

    check conditions accepted

--- src/app/modules/pages/register/register.component.ts
+++ src/app/modules/pages/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidation } from '../../auth-navigation/pipes/customVal';
+
 
 
 @Component({
@@ -8,9 +11,12 @@ import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({})
+  conditions_accepted:boolean = false
 
   constructor(
     private translateService: TranslateService,
+    private formBuilder: FormBuilder
 
   ) { 
     this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
@@ -19,6 +25,23 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  buildForm() {
+    this.registerForm = this.formBuilder.group({
+      email: ['',{updateOn: 'change', validators:[Validators.required,Validators.email]}],
+      password: ['',{updateOn:'change', validators:[Validators.required,CustomValidation.passwordPattern]}],
+      passwordVerify: ['',{updateOn:'change', validators:[Validators.required]}],
+      adult: []
+    })
+  }
+
+  acceptTerms(){
+    this.conditions_accepted = true
+  }
+
+  toggleConditionsAccepted(){
+    this.conditions_accepted = !this.conditions_accepted
   }
 
 }

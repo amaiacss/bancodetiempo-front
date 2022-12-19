583e43634fe02f9d979d5e46c31128f9ceb43921
nereVega <nvega@birt.eus>
2022-12-19 10:47:05 +0100

    impl navegación autorizada

--- src/app/modules/auth-navigation/user/user.component.ts
+++ src/app/modules/auth-navigation/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -9,8 +9,10 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  isLoged:boolean = false
+  userId:string | undefined | null = undefined
 
-  routeParams:{userId:string,selectedProfile:string}={userId:'',selectedProfile:''}
+  selectedProfile:string = ''
   canEdit:boolean = false
 
   constructor(
@@ -18,27 +20,42 @@ export class UserComponent implements OnInit {
     private usersService: UsersService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) { 
+    this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.translateService.use(event.lang);
+    })
+    this.usersService.getSessionData().subscribe(response => {
+      this.userId = response.userData?.id || localStorage.getItem('id')
+      if(this.userId) {
+        this.isLoged = true
+        this.route.params
+      .subscribe(params => {
+        this.selectedProfile = params["profile"]
+    })
+    if(this.userId === this.selectedProfile){
+      this.canEdit = true
+    }else {
+      this.canEdit = false
+    }
+        this.router.navigate([`/user/${this.userId}/profile/${this.selectedProfile}`])
+      }else{
+        this.isLoged = false
+        alert('No tiene permisos')
+        this.router.navigate(['/'])
+      }
+    })
+  }
 
   ngOnInit(): void {
-    this.route.params
-      .subscribe(params => {
-        this.routeParams = {
-          userId:params["id"],
-          selectedProfile:params["profile"]
-        }
-        if(this.routeParams.userId === this.routeParams.selectedProfile){
-          this.canEdit = true
-        }else {
-          this.canEdit = false
-        }
-      }
-    );
-  }
+    
+  } 
+  
 
   goToNewActivitiePage(){
     // route: create
-    this.router.navigate([`/user/${this.routeParams.userId}/create`])
+    if(this.userId){
+      this.router.navigate([`/user/${this.userId}/create`])
+    }
   }
 
 }

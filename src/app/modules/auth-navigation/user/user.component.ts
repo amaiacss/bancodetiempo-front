463b1f324c7edab7c2927d0055ec31e4f1f655cc
nereVega <nvega@birt.eus>
2022-12-19 10:23:24 +0100

    enlace a nueva actividad

--- src/app/modules/auth-navigation/user/user.component.ts
+++ src/app/modules/auth-navigation/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  routeParams:{userId:string,selectedProfile:string}={userId:'',selectedProfile:''}
+  canEdit:boolean = false
+
+  constructor(
+    private translateService: TranslateService,
+    private usersService: UsersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe(params => {
+        this.routeParams = {
+          userId:params["id"],
+          selectedProfile:params["profile"]
+        }
+        if(this.routeParams.userId === this.routeParams.selectedProfile){
+          this.canEdit = true
+        }else {
+          this.canEdit = false
+        }
+      }
+    );
+  }
+
+  goToNewActivitiePage(){
+    // route: create
+    this.router.navigate([`/user/${this.routeParams.userId}/create`])
   }
 
 }

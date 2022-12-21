5c392e640ad6e1f0ef00c9dbbd4203adc809cab9
nereVega <nvega@birt.eus>
2022-12-21 11:34:14 +0100

    user picture or generic

--- src/app/modules/auth-navigation/user/user.component.ts
+++ src/app/modules/auth-navigation/user/user.component.ts
@@ -18,6 +18,7 @@ export class UserComponent implements OnInit {
   canEdit:boolean = false
   fullProfile:boolean = false
 
+  profileContent:any = {}
   profileActivities:CardInfo[] = []
   profileInteractions: [] = []
 
@@ -35,21 +36,25 @@ export class UserComponent implements OnInit {
     this.usersService.getSessionData().subscribe(response => {
       this.userId = response.userData?.id || localStorage.getItem('id')
       this.fullProfile = response.fullProfile || localStorage.getItem('fullProfile')==='true'
+      // Controlar que el usuario que navega está logueado
       if(this.userId) {
         this.isLoged = true
         this.route.params
       .subscribe(params => {
         this.selectedProfile = params["profile"]
+        this.loadData()
         this.profileActivities = this.activitiesService.getProfileActivities(this.selectedProfile)
         this.profileInteractions = this.activitiesService.getProfileInteractions(this.selectedProfile)
     })
+    // Verifica si el usuario logueado está en su propio perfil
       if(this.userId === this.selectedProfile){
         this.canEdit = true
       }else {
         this.canEdit = false
       }
+    //Controla que el usuario no pueda falsear su identidad mediante url
         this.router.navigate([`/user/${this.userId}/profile/${this.selectedProfile}`])
-      }else{
+      }else{  //USUARIO NO LOGUEADO
         this.isLoged = false
         alert('No tiene permisos')
         this.router.navigate(['/'])
@@ -71,5 +76,13 @@ export class UserComponent implements OnInit {
     this.router.navigate([`/user/${this.userId}/preferences`])
   }
 
+  loadData(){
+    this.usersService.getUserProfile(this.selectedProfile).subscribe({
+      next: (data) => {
+        this.profileContent = data[0]
+        console.log(this.profileContent)
+      }
+    })
+  }
 
 }

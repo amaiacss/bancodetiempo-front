fa320b6df4e5acad4226d0bf088967923c251b6b
nereVega <nvega@birt.eus>
2023-01-04 19:35:18 +0100

    scrollRestoration

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { CardInfo } from 'src/app/models/activities';
 import { ActivitiesService } from 'src/app/services/activities.service';
@@ -19,25 +19,40 @@ export class HomeComponent implements OnInit {
     private usersService: UsersService,
     private activitiesService: ActivitiesService,
     private translateService: TranslateService,
+    private route: ActivatedRoute,
     private router: Router
     ) {
       this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
         this.translateService.use(event.lang);
       })
-      this.usersService.getSessionData().subscribe(response => {
-        this.userId = response.userData?.id || localStorage.getItem('id')
-        if(this.userId) {
-          this.isLoged = true
-          this.router.navigate([`/user/${this.userId}`])
-        }else{
-          this.isLoged = false
-          this.router.navigate(['/'])
-        }
-      })
+      
     }
 
   ngOnInit(): void {
     this.activities = this.activitiesService.getLastActivities()
+
+    this.usersService.getSessionData().subscribe(response => {
+      let id, fragment
+      this.userId = response.userData?.id || localStorage.getItem('id')
+      if(this.userId) {
+        this.isLoged = true
+        this.route.params.subscribe({
+          next: (params) => {
+            id = params['id']
+          }
+        })
+        this.route.fragment.subscribe({
+          next: (frag) => {
+            fragment = frag
+          }
+        })
+        fragment ? this.router.navigate([`/user/${this.userId}`], {preserveFragment: true, fragment: fragment}) : this.router.navigate([`/user/${this.userId}`])
+
+      }else{
+        this.isLoged = false
+        fragment ? this.router.navigate(['/'], {preserveFragment: true, fragment: fragment}) : this.router.navigate(['/'])
+      }
+    })
   }
 
 }
